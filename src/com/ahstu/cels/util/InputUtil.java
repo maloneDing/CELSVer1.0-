3b54f77132cb3307b4b13734fb67af5120c4d0f5
maloneDing <dyl572668169@gmail.com>
2017-06-12 15:59:54 +0800

    实现异常输入的控制

---
+++ src/com/ahstu/cels/util/InputUtil.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.ahstu.cels.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @description 此类的描述
+ * @author ahstu 丁云龙
+ * @createDate 下午3:45:38
+ * @version ver1.0
+ */
+public class InputUtil {
+/***********
+ * 根据用户提示，获取一个整数
+ * @param msg 提示信息
+ * @return
+ */
+	public static	int getInt(String msg) {
+		int result=0;
+		try{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			//输出提示信息
+			System.out.print(msg);
+			String line=br.readLine();//把用户的输入当成是字符串，整行读取
+			//把字符串解析成整数
+		
+			if(line!= null && line.trim().length()>0){
+				result=Integer.parseInt(line);
+			}
+			else {
+				System.out.println(">> 输入的全是空白字符、请重新输入");
+				//调用自己
+				return getInt(msg);
+			}
+		}
+		catch (IOException e) {
+			System.out.println(e.getMessage());
+			//回调自己
+			return getInt(msg);
+		}
+
+		catch (RuntimeException e) {
+		System.out.println("读到的不是整数");
+		return getInt(msg);
+		}
+		//返回读取的整数
+		return 	result;
+	}
+}
